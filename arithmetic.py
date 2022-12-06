b5b4d06c5987cb47024132fd5913d3d3bafc74f5
BABEKIZULU <babekizulu@gmail.com>
2022-12-06 20:10:33 +0200

    arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,33 @@
+# ARITHMETIC OPERATIONS
+
+# addition
+print(2 + 2)
+
+# subtraction 
+print(2 - 2)
+
+# multiplication
+print(2 * 2)
+
+# floating point division
+print(10/3)
+
+# integer division
+print(10//3)
+
+# modulus operator
+print(10%5)
+
+# exponents
+print(2**3)
+
+# incrementation
+i = 0
+i+=1
+print(i)
+
+# decrementation
+j = 10
+j-=3
+print(j)
+
